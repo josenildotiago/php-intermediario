eeed04cf0717bc39ca0730eb12a5a22e0255cda7
Josenildo Tiago <josenildo.tiago.designer@gmail.com>
2018-11-24 13:39:16 -0200

    aula foreach

---
+++ aula3-foreach/index.php
@@ -0,0 +1,7 @@
+<?php header("Content-type: text/html; charset=utf-8"); ?>
+<?php
+$nomes = array("Lucas","Matheus","Miguel","Mariana");
+  foreach ($nomes as $luno) {
+      echo "Aluno: ".$luno."<br>";
+  }
+?>
