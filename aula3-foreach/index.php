1e23b533ae3bc4094d45b4fcbd8875751ba5e430
Josenildo Tiago <josenildo.tiago.designer@gmail.com>
2018-11-24 13:50:16 -0200

    exemplo 2

--- aula3-foreach/index.php
+++ aula3-foreach/index.php
@@ -4,4 +4,13 @@ $nomes = array("Lucas","Matheus","Miguel","Mariana");
   foreach ($nomes as $luno) {
       echo "Aluno: ".$luno."<br>";
   }
+//Exemplo 2
+$nomes2 = array(
+  array("nome"=>"Lucas", "idade"=>15),
+  array("nome"=>"Matheus", "idade"=>2),
+  array("nome"=>"Miguel", "idade"=>1)
+);
+  foreach ($nomes2 as $aluno) {
+    echo "Aluno: ".$aluno["nome"]." - Idade: ".$aluno;
+  }
 ?>
