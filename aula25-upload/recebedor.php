ecfc7eab2d0734b88b145e27791172455d0dfc52
Josenildo Tiago <josenildo.tiago.designer@gmail.com>
2018-11-25 16:08:39 -0200

    finalizando

--- aula25-upload/recebedor.php
+++ aula25-upload/recebedor.php
@@ -1,7 +1,16 @@
+<pre>
 <?php
   $arquivo = $_FILES['arquivo'];
+  $ext = $arquivo['type'];
+  $extencao = explode("/",$ext);
+  $final = $extencao[1];
+  //print_r($arquivo);
+  //echo $final;
   if (isset($arquivo['tmp_name']) && !empty($arquivo['tmp_name'])) {
-      move_uploaded_file($arquivo['tmp_name'], 'arquivo/'.$arquivo['name']);
-      header("Location: index.php");
+      $nomedoarquivo = md5(time()).rand(0,99).".".$final;
+      move_uploaded_file($arquivo['tmp_name'], 'arquivo/'.$nomedoarquivo);
+      //header("Location: index.php");
+      echo "Upload realizado com sucesso";
   }
 ?>
+</pre>
