9a53c214b2ab5e7b8167381d46479a0349c7eac4
Josenildo Tiago <josenildo.tiago.designer@gmail.com>
2018-11-25 17:47:50 -0200

    acrescentando condições

--- aula25-upload/recebedor.php
+++ aula25-upload/recebedor.php
@@ -1,16 +1,22 @@
 <pre>
 <?php
   $arquivo = $_FILES['arquivo'];
-  $ext = $arquivo['type'];
-  $extencao = explode("/",$ext);
+  $ext = $arquivo['name'];
+  $ext2 = $arquivo['type'];
+  $extencao = explode(".",$ext);
   $final = $extencao[1];
-  //print_r($arquivo);
-  //echo $final;
+  $cond = explode("/",$ext2);
+  $final2 = $cond[0];
+
+if ($final2 == "image") {
   if (isset($arquivo['tmp_name']) && !empty($arquivo['tmp_name'])) {
-      $nomedoarquivo = md5(time()).rand(0,99).".".$final;
-      move_uploaded_file($arquivo['tmp_name'], 'arquivo/'.$nomedoarquivo);
-      //header("Location: index.php");
-      echo "Upload realizado com sucesso";
+    $nomedoarquivo = md5(time()).rand(0,99).".".$final;
+    move_uploaded_file($arquivo['tmp_name'], 'arquivo/'.$nomedoarquivo);
+    //header("Location: index.php");
+    echo "Upload realizado com sucesso";
   }
+} else {
+  echo "Extenção não permetido";
+}
 ?>
 </pre>
